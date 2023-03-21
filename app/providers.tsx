c03f93591847157e50d2df801a814f109c2c95ea
wajima <mikkywaji@icloud.com>
2023-03-21 18:11:38 +0900

    appディレクトリ用に合わせたMSWを構築

---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {RecoilRoot} from "recoil";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+    require("../lib/mocks");
+  }
+
+  return (
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
+  );
+};
--- lib/mocks/handlers.ts
+++ lib/mocks/handlers.ts
@@ -1,1 +1,44 @@
-export const handlers = [];
+import {graphql} from "msw";
+import {GetUserDocument} from "@/lib/graphql/types/graphql";
+
+const users : {id: string, name: string, email: string}[] = [
+  {
+    id: "1",
+    name: "テストユーザー１",
+    email: "test1@example.com"
+  },
+  {
+    id: "2",
+    name: "テストユーザー１",
+    email: "test1@example.com"
+  },
+  {
+    id: "3",
+    name: "テストユーザー１",
+    email: "test1@example.com"
+  },
+]
+
+export const handlers = [
+  graphql.query(GetUserDocument, (req, res, ctx) => {
+    const {id} = req.variables;
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res(
+        ctx.errors([
+          {
+            message: "User not found",
+          },
+        ])
+      );
+    }
+    return res(
+      ctx.data({
+        user: {
+          __typename: "User",
+          ...user
+        },
+      })
+    );
+  })
+];
